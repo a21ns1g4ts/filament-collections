052711e1f18fdd42715c7aca59b00860ec45c7ab
a21ns1g4ts <atila.danvi@outlook.com>
2025-06-20 04:52:12 -0300

    Update filament collections route to include 'api' prefix in the URL

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,6 @@
 use A21ns1g4ts\FilamentCollections\Http\Controllers\CollectionContentController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/filament-collections', [CollectionContentController::class, 'index'])
+Route::get('api/filament-collections', [CollectionContentController::class, 'index'])
     ->middleware('api')
     ->name('filament-collections.index');

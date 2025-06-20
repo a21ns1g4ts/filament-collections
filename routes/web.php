76e47b7c2ddd8cbe97baf59bae994e1b7cee7f01
a21ns1g4ts <atila.danvi@outlook.com>
2025-06-20 04:25:56 -0300

    Change middleware for filament collections route from 'web' to 'api'

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use A21ns1g4ts\FilamentCollections\Http\Controllers\CollectionContentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/filament-collections', [CollectionContentController::class, 'index'])
-    ->middleware('api')
+    ->middleware('web')
     ->name('filament-collections.index');

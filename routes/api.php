8c567d700ce6bdb696e114c286abfee697b4bbaa
a21ns1g4ts <atila.danvi@outlook.com>
2025-06-23 10:39:01 -0300

    Remove redundant 'api' middleware from filament-collections routes for cleaner configuration

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,8 @@ use A21ns1g4ts\FilamentCollections\Http\Controllers\CollectionContentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('api/filament-collections', [CollectionContentController::class, 'index'])
-    ->middleware('api')
     ->name('filament-collections.index');
 
 Route::post('api/filament-collections', [CollectionContentController::class, 'store'])
-    ->middleware(['api', 'auth:sanctum', 'abilities:create'])
+    ->middleware(['auth:sanctum', 'abilities:create'])
     ->name('filament-collections.store');

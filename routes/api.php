691304071823d2554fcc5c0e920314010621f738
a21ns1g4ts <atila.danvi@outlook.com>
2025-06-23 10:41:17 -0300

    Comment out filament-collections routes for future reference and potential reactivation

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,9 @@
 use A21ns1g4ts\FilamentCollections\Http\Controllers\CollectionContentController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('api/filament-collections', [CollectionContentController::class, 'index'])
-    ->name('filament-collections.index');
+// Route::get('api/filament-collections', [CollectionContentController::class, 'index'])
+//     ->name('filament-collections.index');
 
-Route::post('api/filament-collections', [CollectionContentController::class, 'store'])
-    ->middleware(['auth:sanctum', 'abilities:create'])
-    ->name('filament-collections.store');
+// Route::post('api/filament-collections', [CollectionContentController::class, 'store'])
+//     ->middleware(['auth:sanctum', 'abilities:create'])
+//     ->name('filament-collections.store');
